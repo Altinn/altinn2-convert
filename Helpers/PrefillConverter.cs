6d93d827ac542f66a9a75d608c242d9d7cf6fc64
Ivar Nesje <ivarne@users.noreply.github.com>
2022-02-17 09:45:39 +0100

    Fix headers L=>h2, add page header on summary, skip componettypes in summary (#15)
    * Fix headers L=>h2, add page header on summary, skip componettypes in summary
    
    * Improve prefill converter

--- Helpers/PrefillConverter.cs
+++ Helpers/PrefillConverter.cs
@@ -8,7 +8,7 @@ namespace Altinn2Convert.Helpers
 {
     public static class PrefillConverter
     {
-        public static Prefill Convert(FormFieldPrefill prefill)
+        public static Prefill Convert(FormFieldPrefill a2prefill)
         {
             var ret = new Prefill()
             {
@@ -16,34 +16,62 @@ namespace Altinn2Convert.Helpers
                 DSF = new (),
                 UserProfile = new (),
             };
-            prefill?.Register?.FirstOrDefault(r => r.Name == "ER")?.Field?.ForEach(field =>
-            {
-                // var path = XpathToJsonPath(field.XPath);
-                // switch (field.RegisterField)
-                // {
-                //     // TODO: Find all prefill codes in altinn2 er
-                //     // case "SocialSecurityNumber":
-                //     //     return ret.ER.
-                // }
-            });
-            prefill?.Register?.FirstOrDefault(r => r.Name == "DSF")?.Field?.ForEach(field =>
+            a2prefill?.Register?.FirstOrDefault(r => r.Name == "ER")?.Field?.ForEach(field =>
             {
                 var path = XpathToJsonPath(field.XPath);
+                if (path.StartsWith("@my"))
+                {
+                    return;
+                }
+
+// ChosenOrgNr" registerField="BusinessAddress" />
+// ChosenOrgNr" registerField="BusinessPostalCity" />
+// ChosenOrgNr" registerField="BusinessPostalCode" />
+// ChosenOrgNr" registerField="Name" />
+// ChosenOrgNr" registerField="OrganizationNumber" />
+// ChosenOrgNr" registerField="PostalAddress" />
+// ChosenOrgNr" registerField="PostalPostalCity" />
+// ChosenOrgNr" registerField="PostalPostalCode" />
                 switch (field.RegisterField)
                 {
                     // TODO: Find all prefill codes in altinn2 er
-                    case "SocialSecurityNumber":
-                        ret.DSF.SSN = path;
+                    case "OrganizationNumber":
+                        ret.ER.OrgNumber = path;
                         break;
-                    case "FirstName":
-                        ret.DSF.FirstName = path;
+                    case "Name":
+                        ret.ER.Name = path;
                         break;
-                    case "MiddleName":
-                        ret.DSF.MiddleName = path;
+                    case "PostalAddress":
+                        ret.ER.MailingAddress = path;
                         break;
-                    case "LastName":
-                        ret.DSF.LastName = path;
+                    case "PostalPostalCode":
+                        ret.ER.MailingPostalCode = path;
                         break;
+                    case "PostalPostalCity":
+                        break;
+                }
+            });
+            a2prefill?.Register?.FirstOrDefault(r => r.Name == "DSF")?.Field?.ForEach(field =>
+            {
+                var path = XpathToJsonPath(field.XPath);
+                if (path.StartsWith("@my"))
+                {
+                    return;
+                }
+
+// Person" registerField="Address" />
+// Person" registerField="FirstName" />
+// Person" registerField="LastName" />
+// Person" registerField="MiddleName" />
+// Person" registerField="Name" />
+// Person" registerField="PlaceName" />
+// Person" registerField="PostalCity" />
+// Person" registerField="PostalCode" />
+// Person" registerField="SocialSecurityNumber" />
+// Person" registerField="StreetName" />
+                switch (field.RegisterField)
+                {
+                    // TODO: Find all prefill codes in altinn2 er
                     case "Address":
                         // TODO: figure out what is right here
                         //       all of theese seems to match a single field in Altinn2
@@ -51,38 +79,62 @@ namespace Altinn2Convert.Helpers
                         ret.DSF.AddressHouseNumber = path;
                         ret.DSF.AddressHouseLetter = path;
                         break;
-                    case "PostalCode":
-                        ret.DSF.AddressPostalCode = path;
-                        ret.DSF.MailingPostalCode = path;
+                    case "FirstName":
+                        ret.DSF.FirstName = path;
                         break;
-                    case "PostalCity":
-                        ret.DSF.AddressCity = path;
-                        ret.DSF.MailingPostalCity = path;
+                    case "LastName":
+                        ret.DSF.LastName = path;
+                        break;
+                    case "MiddleName":
+                        ret.DSF.MiddleName = path;
+                        break;
+                    case "Name":
+                        ret.DSF.Name = path;
                         break;
                     case "PlaceName":
                         ret.DSF.AddressCity = path;
-                        ret.DSF.MailingPostalCity = path;
+                        // ret.DSF.MailingPostalCity = path;
+                        break;
+                    case "PostalCity":
+                        ret.DSF.AddressCity = path;
+                        // ret.DSF.MailingPostalCity = path;
+                        break;
+                    case "PostalCode":
+                        ret.DSF.AddressPostalCode = path;
+                        // ret.DSF.MailingPostalCode = path;
+                        break;
+                    case "SocialSecurityNumber":
+                        ret.DSF.SSN = path;
                         break;
                     // TOOD: Complete list
                 }
             });
-            prefill?.Register?.FirstOrDefault(r => r.Name == "DLS")?.Field?.ForEach(field =>
+            a2prefill?.Register?.FirstOrDefault(r => r.Name == "DLS")?.Field?.ForEach(field =>
             {
                 var path = XpathToJsonPath(field.XPath);
+                if (path.StartsWith("@my"))
+                {
+                    return;
+                }
+
+// Profile" registerField="ProfileEmail" />
+// Profile" registerField="ProfileLastName" />
+// Profile" registerField="ProfileMobile" />
+// Profile" registerField="ProfileSSN" />
                 switch (field.RegisterField)
                 {
                     // TODO: Fix mapping
-                    case "PostalCity":
-                        ret.UserProfile.PartyPersonAddressCity = path;
-                        ret.UserProfile.PartyPersonMailingPostalCity = path;
-                        ret.UserProfile.PartyOrganizationMailingPostalCity = path;
-                        ret.UserProfile.PartyOrganizationBusinessPostalCity = path;
+                    case "ProfileEmail":
+                        ret.UserProfile.Email = path;
                         break;
-                    case "PlcaceName":
-                        ret.UserProfile.PartyPersonAddressCity = path;
-                        ret.UserProfile.PartyPersonMailingPostalCity = path;
-                        ret.UserProfile.PartyOrganizationMailingPostalCity = path;
-                        ret.UserProfile.PartyOrganizationBusinessPostalCity = path;
+                    case "ProfileLastName":
+                        ret.UserProfile.PartyPersonLastName = path;
+                        break;
+                    case "ProfileMobile":
+                        ret.UserProfile.PartyPersonMobileNumber = path;
+                        break;
+                    case "ProfileSSN":
+                        ret.UserProfile.PartySSN = path;
                         break;
                 }
             });
